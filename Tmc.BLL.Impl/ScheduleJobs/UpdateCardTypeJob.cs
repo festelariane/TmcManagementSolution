20efb6d9e19e94f88629a6f53f48546efdad5251
festelariane <trinhthaituyen2003@yahoo.com>
2014-09-12 17:47:26 +0700

    Add Quartz

---
+++ Tmc.BLL.Impl/ScheduleJobs/UpdateCardTypeJob.cs
@@ -0,0 +1,17 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tmc.BLL.Impl.ScheduleJobs
+{
+    public class UpdateCardTypeJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            
+        }
+    }
+}
